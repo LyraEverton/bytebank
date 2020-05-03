2f3e229abc799abb49fe26f4dae0861136c75436
Everton Lyra <everton.lyra@outlook.com>
2020-05-03 16:15:04 -0300

    Cards and ListTile

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('100'),
+              subtitle: Text('1000'),
+              )
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('100'),
+              subtitle: Text('1000'),
+              )
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    ) ,
+  ));
+}
