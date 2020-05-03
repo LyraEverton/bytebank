825885189628c3025ed62b82ebdd2fccef4e9869
Everton Lyra <everton.lyra@outlook.com>
2020-05-03 17:35:20 -0300

    refactoring and inheritance

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,49 @@ void main() {
       appBar: AppBar(
         title: Text('Transferências'),
       ),
-      body: Column(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100'),
-              subtitle: Text('1000'),
-              )
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100'),
-              subtitle: Text('1000'),
-              )
-          ),
-        ],
-      ),
+      body: ListaTransferencia(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ),
     ) ,
   ));
 }
+
+class ListaTransferencia extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+  return Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100.0, 1000)),
+          ItemTransferencia(Transferencia(120.0, 100220)),
+          ItemTransferencia(Transferencia(130.0, 3211)),
+        ],
+      );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget{
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+  }
