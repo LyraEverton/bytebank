ba7d34a5172594f2b4eb1d230429dc4649be8a71
Everton Lyra <everton.lyra@outlook.com>
2020-05-04 18:34:58 -0300

    TextFields, Form and Submit Button - Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,81 @@
 import 'package:flutter/material.dart';
 //
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+void main() =>runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Transferências'),
+      title: Text('Criando Área de Transferência'), 
       ),
-      body: ListaTransferencia(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
-    ) ,
-  ));
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0
+              ),
+              decoration: InputDecoration(
+                labelText: 'Account Number',
+                hintText: 'XXXX',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Value',
+                hintText: 'XXXX',
+              ),
+                
+            ),
+          ),
+          RaisedButton(
+            child:Text('Submit'),
+          )
+        ],
+      )  , 
+    );
+  }
 }
 
 class ListaTransferencia extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-  return Column(
+  return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
         children: <Widget>[
           ItemTransferencia(Transferencia(100.0, 1000)),
           ItemTransferencia(Transferencia(120.0, 100220)),
           ItemTransferencia(Transferencia(130.0, 3211)),
         ],
-      );
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
 
@@ -31,7 +83,6 @@ class ItemTransferencia extends StatelessWidget{
   final Transferencia _transferencia;
 
   ItemTransferencia(this._transferencia);
-
   
   @override
   Widget build(BuildContext context) {
