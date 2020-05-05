efa023372cff3326aeb4fc155a642dad66b6c1e0
Everton Lyra <everton.lyra@outlook.com>
2020-05-05 12:09:40 -0300

    onPress a rocoverying data from textFields

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,9 @@ class BytebankApp extends StatelessWidget {
 }
 
 class FormularioTransferencia extends StatelessWidget {
+  
+  final TextEditingController _controladorCampoNumeroConta = TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,12 +28,13 @@ class FormularioTransferencia extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
+              controller: _controladorCampoNumeroConta,
               style: TextStyle(
                 fontSize: 24.0
               ),
               decoration: InputDecoration(
                 labelText: 'Account Number',
-                hintText: 'XXXX',
+                hintText: '1234',
               ),
               keyboardType: TextInputType.number,
             ),
@@ -38,19 +42,32 @@ class FormularioTransferencia extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
+              controller:_controladorCampoValor,
               style: TextStyle(
                 fontSize: 24.0,
               ),
               decoration: InputDecoration(
                 icon: Icon(Icons.monetization_on),
                 labelText: 'Value',
-                hintText: 'XXXX',
+                hintText: '0.00',
               ),
                 
             ),
           ),
           RaisedButton(
             child:Text('Submit'),
+            onPressed: () { 
+              print('Clicou');
+              final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+              final double valor = double.tryParse(_controladorCampoValor.text);
+
+              if(numeroConta != null && valor != null){
+                final transferenciaCriada =  Transferencia(valor, numeroConta);
+                debugPrint('$transferenciaCriada');
+              }
+              
+             },
+            
           )
         ],
       )  , 
@@ -102,4 +119,10 @@ class Transferencia {
   final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString(){
+    return 'transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
+
   }
